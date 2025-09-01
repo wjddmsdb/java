1fdfc234e28d8471aa80a84809837cf58675638b
wjddmsdb <eunyuj648@gmail.com>
2025-09-01 12:20:18 +0900

    9월 1일 수업

---
+++ chap04/src/sec02/exam02/ForSumFrom1To100Example1.java
@@ -0,0 +1,15 @@
+package sec02.exam02;
+
+public class ForSumFrom1To100Example1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int sum = 0;
+		
+		for(int i = 1; i<=100; i++) {
+			sum += i;
+		}
+		System.out.println("1~100 합 : " + sum);
+	}
+
+}
