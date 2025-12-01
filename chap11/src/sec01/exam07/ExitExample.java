8ccfbd53214b65e6b8108787cff1447f151dbcf9
wjddmsdb <eunyuj648@gmail.com>
2025-12-01 12:30:23 +0900

    12월 1일 수업

---
+++ chap11/src/sec01/exam07/ExitExample.java
@@ -0,0 +1,15 @@
+package sec01.exam07;
+
+public class ExitExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i = 0; i< 10; i++) {
+			if(i == 5) {
+				System.exit(0);
+			}
+		}
+		System.out.println("마무리 코드");
+	}
+
+}
---
+++ chap11/src/sec01/exam09/ClassExample.java
@@ -0,0 +1,22 @@
+package sec01.exam09;
+
+public class ClassExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//how1
+		Class clazz = Car.class;
+		
+		//how2
+		//Class clazz = Class.forName("sec01.exam09.Car");
+		
+		//how3
+		//Car car = new Car();
+		//Class clazz = car.getClass();
+		
+		System.out.println(clazz.getName());
+		System.out.println(clazz.getSimpleName());
+		System.out.println(clazz.getPackage().getName());
+	}
+
+}
