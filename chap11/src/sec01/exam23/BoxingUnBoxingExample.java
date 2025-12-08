3e764f5c3eff733a4c67b390b76f88e8e6e9517e
wjddmsdb <eunyuj648@gmail.com>
2025-12-08 15:06:58 +0900

    12월 8일

---
+++ chap11/src/sec01/exam23/BoxingUnBoxingExample.java
@@ -0,0 +1,20 @@
+package sec01.exam23;
+
+public class BoxingUnBoxingExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer obj1 = new Integer(100);
+		Integer obj2 = new Integer("200");
+		Integer obj3 = Integer.valueOf("300");
+		
+		int value1 = obj1.intValue();
+		int value2 = obj2.intValue();
+		int value3 = obj3.intValue();
+		
+		System.out.println(value1);
+		System.out.println(value2);
+		System.out.println(value3);
+	}
+
+}
---
+++ chap11/src/sec01/exam28/MathRandomExample.java
@@ -0,0 +1,11 @@
+package sec01.exam28;
+
+public class MathRandomExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = (int) (Math.random()*6) + 1;
+		System.out.println("주사위 눈: " + num);
+	}
+
+}
